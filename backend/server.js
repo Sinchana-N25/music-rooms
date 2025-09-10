1ce771b2325c6b3bcf452d9863fb7e8113eb179f
Sinchana N <sinchana2505@gmail.com>
2025-09-10 15:35:34 +0530

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -23,7 +23,7 @@ app.use(
     saveUninitialized: true,
     cookie: {
       secure: process.env.NODE_ENV === "production",
-      sameSite: "none",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     },
   })
 );
